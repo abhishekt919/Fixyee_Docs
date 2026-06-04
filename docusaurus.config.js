f85f551840ffb91b44cf10ef5dae3d7213040d77
Rajesh <abhishek@smallarc.com>
2026-06-05 01:34:35 +0530

    changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,12 +46,8 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+  sidebarPath: './sidebars.js',
+},
         blog: {
           showReadingTime: true,
           feedOptions: {
@@ -95,12 +91,12 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          //{to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+            to: "/ApiReference",
+            label: "API Reference",
+            position: "left",
+          }
+          //{to: '/blog', label: 'Blog', position: 'left'},
         ],
       },
       footer: {
